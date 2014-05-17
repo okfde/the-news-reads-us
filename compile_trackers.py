4fa43461e3a353058179b521b260f1fc9797ecf4
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2014-05-17 03:55:36 +0200

    still only 88 of 187 trackers with ghostery data

--- compile_trackers.py
+++ compile_trackers.py
@@ -2,33 +2,39 @@ import json
 import csv
 
 from collections import defaultdict
+from urlparse import urlparse
 
 FILE_NAMES = 'data/ghostery.json'
 FILE_DOMAINS = 'data/tracker_domains.csv'
 
 
 def parse_trackers():
-    domains_info = {}
+    domainsInfo = {}
     with open(FILE_DOMAINS, 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            name = row[0].strip().split('.', 1)[0]
-            domains_info[name] = {}
-            domains_info[name]['domain']=row[0].strip()
-            domains_info[name]['count']=row[1]
-
+            domainsInfo[row[0]] = {}
+            domainsInfo[row[0]]['count']=row[1]
+        print len(domainsInfo)
     with open(FILE_NAMES, 'rb') as fh:
-        ghostery_data = json.load(fh)
+        ghosteryData = json.load(fh)
 
-        keys = ['company_description', 'company_in_their_own_words', 'affiliation_groups','company_website_url']
-
-        for item in ghostery_data:
-            if item in domains_info:
-                for key in keys:
-                    domains_info[item][key] = ghostery_data[item][key]
-
-    with open('tracker_sites.json', 'wb') as fh:
-        json.dump(domains_info, fh, indent=2)
+        keys = ['company_description','company_name', 'company_in_their_own_words', 'affiliation_groups','company_website_url']
+        count = 0
+        for domainItem in domainsInfo:
+            for ghItem in ghosteryData:
+                ghUrl = ghosteryData[ghItem].get('company_website_url')
+                if ghUrl!=None:
+                    ghUrl = urlparse(ghUrl).netloc
+                    if domainItem in ghUrl:
+                        count = count +1
+                        for key in keys:
+                            if key in ghosteryData[ghItem]:
+                                #print ghItem, ghUrl, domainItem
+                                domainsInfo[domainItem][key] = ghosteryData[ghItem][key]
+        print count
+        with open('tracker_sites.json', 'wb') as fh:
+            json.dump(domainsInfo, fh, indent=2)
 
 
 if __name__ == '__main__':
